0f6b4282b4759c20afb08336677658ba2822e75f
abhay <abhaytxanax@gmail.com>
2023-07-12 14:38:13 +0530

    overlay-ui-completed

--- src/components/header/MainNavigation.js
+++ src/components/header/MainNavigation.js
@@ -13,12 +13,13 @@ const MainNavigation = () => {
       <div className='h-[70px] bg-[#0D253F] flex px-16 items-center justify-between'>
         <div className='flex'>
           <h1 className='text-4xl tracking-wider font-bold '>Name</h1>
-          <button
-            className='ml-2 w-[172px] h-[48px] bg-[#171D21] rounded-lg text-[18px] relative'
+          <div
+            className='ml-2 w-[172px] h-[48px] bg-[#171D21] rounded-lg flex items-center justify-center'
             onClick={() => {dispatch(createOverlayActions.setShowOverlay(true))}}
           >
-            Projects <IoMdArrowDropdown className='absolute right-8 top-4' />
-          </button>
+            <p className='text-lg'>Projects</p>  
+            <IoMdArrowDropdown className=' right-8 top-4' />
+          </div>
         </div>
         <div className='flex'>
           <Link className='text-gray-300 hover:text-gray-50' to={'/'}>Profile</Link>
--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,12 +1,19 @@
 import { Outlet } from "react-router-dom"
+import { useSelector } from "react-redux"
+
 import MainNavigation from "../components/header/MainNavigation"
 import AsideLeft from "../components/aside/AsideLeft"
 import AsideRight from "../components/aside/AsideRight"
 import Main from "../components/main/Main"
+import Overlay from "../components/UI/Overlay"
 
 const Root = () => {
+
+  const overlay = useSelector(state => state.createOverlay.showOverlay);
+
   return (
     <>
+      {overlay && <Overlay />}
       <MainNavigation />
       <main className="flex justify-between">
         <AsideLeft  />
